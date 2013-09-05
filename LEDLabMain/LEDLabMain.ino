a20717f67334fd5a5914f7b7f87507486ef2d358
Daria <dnesterovich2014@gmail.com>
2013-09-05 18:35:43 -0400

    Final Working Code!
    With extensive commenting :)

--- LEDLabMain/LEDLabMain.ino
+++ LEDLabMain/LEDLabMain.ino
@@ -1,25 +1,21 @@
-/*
- Fade
- 
- This example shows how to fade an LED on pin 9
- using the analogWrite() function.
- 
- This example code is in the public domain.
- */
+// Team Zephyrus
+// Daria, Jessica, Trisha, Kristie
+
+// Code modified from Fade (Arduino Example Code)
 
 int led = 9;           // the pin that the LED is attached to
 int upbutt = 4;        // increase brightness button
 int downbutt = 2;      // decrease brightness button
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+float brightness = 0;    // how bright the LED is
+float fadeAmount = 0.25;    // how many points to fade the LED by
 int output;
 
 // the setup routine runs once when you press reset:
 void setup()  { 
-  // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
-  pinMode(upbutt, INPUT);
-  pinMode(downbutt, INPUT);
+  
+  pinMode(led, OUTPUT);      // declare pin 9 to be an output:
+  pinMode(upbutt, INPUT);    // declare pin 4 to be an input:
+  pinMode(downbutt, INPUT);  // declare pin 2 to be an input:
   
   Serial.begin(9600);
 } 
@@ -28,34 +24,29 @@ void setup()  {
 void loop()  { 
   
   // call read button function
-  output = read_buttons();
-  Serial.println(output);   
+  output = read_buttons(brightness);  
   
-  // change the brightness for next time through the loop:
-  if (brightness >= 5) {
-    brightness = brightness + output*fadeAmount;
-    Serial.println(brightness);
-  }
+  // adjust brightness. output = 1 if up button, 
+  // output = -1 if down button, output = 0 if both or no buttons
+  brightness = brightness + output*fadeAmount;
 
   // set the brightness of pin 9:
-  analogWrite(led, brightness); 
+  analogWrite(led, brightness);   // writes brightness "analogly" to led
   
-  // case
-  
+  // stuck in while loop once max brightness level was reached
   while (brightness >= 255) {
-     flashing();
-     delay(5);
+     flashing();    // call flashing function bc max val has been reached
   }
-  
   
   // wait for 30 milliseconds to see the dimming effect    
   delay(30);                            
 }
 
+// flashing function when LED reaches max value
 void flashing() {
-  digitalWrite(led, HIGH);
+  digitalWrite(led, HIGH);  // write ON val to LED
   delay(500);
-  digitalWrite(led, LOW); 
+  digitalWrite(led, LOW);   // write ON val to LED
   delay(500);
 }
 
--- LEDLabMain/ReadButton.ino
+++ LEDLabMain/ReadButton.ino
@@ -1,18 +1,18 @@
-int read_buttons()
+int read_buttons(float brightness)
 {
-  int upval = digitalRead(upbutt);
-  int downval = digitalRead(downbutt);
+  int upval = digitalRead(upbutt);            // read up button value (HIGH/LOW)
+  int downval = digitalRead(downbutt);        // read down button value (HIGH/LOW)
   
-  if (upval == HIGH) {
-     output = 1;
-  }
-  else if (downval == HIGH) {
-     output = -1;
-  }
-  else if (downval == HIGH && upval == HIGH) {
+  if (downval == HIGH && upval == HIGH) {     //case where both buttons pushed
      output = 0;
   }
-  else {
+  else if (upval == HIGH) {                   // case where up button is pushed
+     output = 1;
+  }
+  else if (downval == HIGH && brightness > 0) { // case where down button is pushed
+     output = -1;                               // and brightness is non-zero/negative
+  }
+  else {                                      // case where no button was pressed
      output = 0;
   }  
  
